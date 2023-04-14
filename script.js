9039e312e8bf37de618c27d973d4c586add559ad
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-14 11:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+<script>
+      const sizeInfo = document.getElementById("sizeInfo");
+      const updateSize = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        sizeInfo.innerHTML = `<h1>Width: ${width} Height: ${height}</h1>`;
+      };
+      window.addEventListener("resize", updateSize);
+      updateSize();
+</script>
+  
